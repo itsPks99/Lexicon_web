67eff48748e20cd1fdf799d0de2cad828dc63415
itsPks99 <singhpradum27121999@gmail.com>
2025-03-24 13:27:37 +0530

    second commit

--- src/pages/Home/StatisticsSection/StatisticsSection.jsx
+++ src/pages/Home/StatisticsSection/StatisticsSection.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import './statisticsSection.css';
+import './StatisticsSection.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
