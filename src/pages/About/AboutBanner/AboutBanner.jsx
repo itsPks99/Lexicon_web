0f1a3d599e9905a74aeb314e8bcf1c785abd813e
itsPks99 <singhpradum27121999@gmail.com>
2025-03-26 15:38:58 +0530

    Complate Home, About and Contact under dev after add banner feed

--- src/pages/About/AboutBanner/AboutBanner.jsx
+++ src/pages/About/AboutBanner/AboutBanner.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import "./aboutBanner.css";
+import "./AboutBanner.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
